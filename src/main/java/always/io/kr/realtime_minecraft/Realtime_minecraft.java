a51c49905927aaec913453ec957327a06e79baa5
tsukino_sie <dduri8674@icloud.com>
2025-12-22 21:53:26 +0900

    start

---
+++ src/main/java/always/io/kr/realtime_minecraft/Realtime_minecraft.java
@@ -0,0 +1,17 @@
+package always.io.kr.realtime_minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Realtime_minecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
